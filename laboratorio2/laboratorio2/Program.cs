96c39fc09da7c2002105ca7c739b37020fa80ebb
Jorge Mendoza <georgejorgemendoza@hotmail.com>
2025-09-15 19:22:09 -0500

    Laboratorio 2 Ejemplo 1 y ejemplo 2

---
+++ laboratorio2/laboratorio2/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Client client = new Client();
+        client.FirstName = "Jorge";
+        client.LastName = "Mendoza";
+        client.Age = 25;
+        client.Id = 1;
+
+        Console.WriteLine(client.GetFulName());
+    }
+}
+class Client
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string GetFulName()
+    {
+        return FirstName + " " + LastName;
+    }
+}
---
+++ laboratorio2/laboratorio22/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyClass.Valor = 1;
+        Console.WriteLine(MyClass.Valor);
+    }
+}
+public class MyClass
+{
+    public static int Valor;
+}

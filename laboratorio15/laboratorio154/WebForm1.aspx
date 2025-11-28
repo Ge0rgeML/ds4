27b641e5b89fb8265267868bcb521f2095776ed0
Jorge Mendoza <georgejorgemendoza@hotmail.com>
2025-11-28 18:52:07 -0500

    LABORATORIO 15 CON 4 PROYECTOS COMPLETADO

---
+++ laboratorio15/laboratorio154/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="laboratorio154.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            SUMAR DOS NUMEROS<br />
+            <br />
+            NUMERO 1:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            NUMERO 2:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="SUMAR" />
+&nbsp;&nbsp;&nbsp; RESULTADO:<asp:Label ID="Label1" runat="server"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ laboratorio15/laboratorio154/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Globalization;
+
+namespace laboratorio154
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double value1, value2;
+
+            var s1 = (TextBox1?.Text ?? string.Empty).Trim();
+            var s2 = (TextBox2?.Text ?? string.Empty).Trim();
+
+            var style = NumberStyles.Float | NumberStyles.AllowThousands;
+            var culture = CultureInfo.CurrentCulture;
+
+            if (double.TryParse(s1, style, culture, out value1) &&
+                double.TryParse(s2, style, culture, out value2))
+            {
+                var sum = value1 + value2;
+                Label1.Text = sum.ToString(culture);
+            }
+            else
+            {
+                Label1.Text = "Entrada no válida";
+            }
+        }
+    }
+}
